f6c069b1470ddafef6a69f6f1313432e761a242a
Vikram Naik <26vikramnaik@gmail.com>
2024-09-12 15:46:38 +0530

    feat: create a api for the tresure

---
+++ src/main/java/com/vikram/TechTresure/constants/TechTresureConstants.java
@@ -0,0 +1,10 @@
+package com.vikram.TechTresure.constants;
+
+/**
+ * TechTresureConstants
+ */
+public class TechTresureConstants {
+
+  public static final String TECH_TRESURE = "TechTresure";
+  
+}
---
+++ src/main/java/com/vikram/TechTresure/service/TechTresureService.java
@@ -0,0 +1,12 @@
+package com.vikram.TechTresure.service;
+
+import org.springframework.core.io.InputStreamResource;
+
+/**
+ * TechTresureService
+ */
+public interface TechTresureService {
+
+  public InputStreamResource getTechTresure();
+  
+}
