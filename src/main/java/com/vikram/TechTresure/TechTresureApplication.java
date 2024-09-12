54e420f9551b36c61d8caf7414491b4e107fd4f5
Vikram Naik <26vikramnaik@gmail.com>
2024-09-12 14:28:51 +0530

    init: create a starter project

---
+++ src/main/java/com/vikram/TechTresure/TechTresureApplication.java
@@ -0,0 +1,13 @@
+package com.vikram.TechTresure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTresureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechTresureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vikram/TechTresure/TechTresureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikram.TechTresure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTresureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
